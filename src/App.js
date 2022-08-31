bb4d2cb750a7b757f4fc61f49e10f2cc5343ec22
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-08-30 22:59:03 -0500

    Se agrega footer con errores de tamño

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import AppRouter from "../src/routers/AppRouter"
+import Footer from './components/Footer'
+
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 
 const App = () => {
-  return <AppRouter />;
+  return (
+    <>
+      <AppRouter />
+      <Footer/>
+    </>
+  );
 }
 
 export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+    <>
+<footer className="bg-white">
+  
+
+  <div className="bg-dark py-2">
+    <div className="text-center">
+      <p className="text-muted mb-0 py-2">© 2022  All rights reserved. create by Cesar Reyes</p>
+    </div>
+  </div>
+</footer>
+
+      
+    </>
+  );
+}
+
+export default Footer
