1575c549455eaf4ebc1c487f77da04ebdc022494
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-08-30 22:58:34 -0500

    Se agregan los componentes a la pagina Inciio

--- src/Pages/RegistroInicio.jsx
+++ src/Pages/RegistroInicio.jsx
@@ -1,102 +1,24 @@
 import React from "react";
 import "../styles/form.css";
+import FormSigIn from "../components/FormSingIn";
+import FormSigUp from "../components/FormSingUp";
 
 const RegistroInicio = () => {
   return (
     <>
-      <main className="container  py-5 ">
+      <main className="container  my-5 py-5">
         <h2 className="text-center mb-5">Vamos registrate o inicia sesion</h2>
         <main className="row pruebaform">
           <section className="col-md-6 ">
-            <h4 className="text-center">Form1</h4>
+            <h4 className="text-center">Inicia Sesion</h4>
             <article>
-              <form>
-                <div className="mb-3">
-                  <label htmlFor="exampleInputEmail1" className="form-label">
-                    Email address
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="exampleInputPassword1" className="form-label">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="exampleInputPassword1"
-                  />
-                </div>
-                <div className="mb-3 form-check">
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    id="exampleCheck1"
-                  />
-                  <label className="form-check-label" htmlFor="exampleCheck1">
-                    Check me out
-                  </label>
-                </div>
-                <div className=" text-center">
-                  <button
-                    type="submit"
-                    className=" text-center  btn btn-primary"
-                  >
-                    Submit
-                  </button>
-                </div>
-              </form>
+              <FormSigIn />
             </article>
           </section>
           <section className="col-md-6">
-            <h4 className="text-center">Form2</h4>
+            <h4 className="text-center">Registrate</h4>
             <article>
-              <form>
-                <div className="mb-3">
-                  <label htmlFor="exampleInputEmail1" className="form-label">
-                    Email address
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="exampleInputPassword1" className="form-label">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="exampleInputPassword1"
-                  />
-                </div>
-                <div className="mb-3 form-check">
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    id="exampleCheck1"
-                  />
-                  <label className="form-check-label" htmlFor="exampleCheck1">
-                    Check me out
-                  </label>
-                </div>
-                <div className=" text-center">
-                  <button
-                    type="submit"
-                    className=" text-center  btn btn-primary"
-                  >
-                    Submit
-                  </button>
-                </div>
-              </form>
+              <FormSigUp />
             </article>
           </section>
         </main>
