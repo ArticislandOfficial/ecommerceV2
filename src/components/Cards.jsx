2b2ed42bae2757788b9ccb456da98e0cabfb9d7e
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-09-03 12:31:27 -0500

    Creando componente cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,19 +3,21 @@ import React from 'react'
 const Cards = (props) => {
   return (
     <>
-      <div className="card " style={{ width: "18rem" }}>
-        <img
-          src={props.src}
-          className="card-img-top" //https://picsum.photos/1080
-          alt={props.alt}
-        />
-        <div className="card-body text-center">
-          <h5 className="card-title">{props.Cardtitle}</h5>
-          <p className="card-text">{props.text}</p>
-          <a href={props.href} className="btn btn-primary">
-            {props.textButton}
-          </a>
-        </div>
+      <div className={props.class}>
+        <section className="card estiloextcard  my-3 ">
+          <img
+            src={props.src}
+            className="card-img-top" //https://picsum.photos/1080
+            alt={props.alt}
+          />
+          <article className="card-body text-center">
+            <h5 className="card-title">{props.Cardtitle}</h5>
+            <p className="card-text">{props.text}</p>
+            <a href={props.href} className="btn btn-primary">
+              {props.textButton}
+            </a>
+          </article>
+        </section>
       </div>
     </>
   );
