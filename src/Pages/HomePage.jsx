4166cf4fa31aecf44f905f194f771b70b1c92661
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-09-03 12:33:59 -0500

    Terminando  homepage y Productos

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,48 +1,50 @@
 import React from "react";
-import Cards  from "../components/Cards";
+import Cards from "../components/Cards";
 const HomePage = () => {
   return (
     <>
       <section className="hero">
         <article className="contenido-hero">
-          <h2>Servicios de impresion , diseño y venta de modelos 3D</h2>
+          <h2 className="py-1 px-2 text-center">
+            Servicios de impresion , diseño y venta de modelos 3D
+          </h2>
           <button type="button" class="boton btn btn-success">
             Ver catalogo
           </button>
         </article>
       </section>
-      <section className="row">
-        <h3>Top de Productos</h3>
-        <article className="col-md-4 ">
-          <Cards
-            src="https://picsum.photos/1080"
-            alt="imagen1"
-            Cardtitle="Producto 1"
-            text="20 USD"
-            href=""
-            textButton="Ver Producto"
-          />
-        </article>
-        <article className="col-md-4">
-          <Cards
-            src="https://picsum.photos/1080"
-            alt="imagen1"
-            Cardtitle="Producto 1"
-            text="50 USD"
-            href=""
-            textButton="Ver Producto"
-          />
-        </article>
-        <article className="col-md-4">
-          <Cards
-            src="https://picsum.photos/1080"
-            alt="imagen1"
-            Cardtitle="Producto 1"
-            text="80 USD"
-            href=""
-            textButton="Ver Producto"
-          />
-        </article>
+      <section className="row ">
+        <h3 className="my-3">Top de Productos</h3>
+
+        <Cards
+          class="col-md-4"
+          src="https://picsum.photos/1080"
+          alt="imagen1"
+          Cardtitle="Producto 1"
+          text="20 USD"
+          href=""
+          textButton="Ver Producto"
+        />
+
+        <Cards
+          class="col-md-4"
+          src="https://picsum.photos/1080"
+          alt="imagen1"
+          Cardtitle="Producto 1"
+          text="50 USD"
+          href=""
+          textButton="Ver Producto"
+        />
+
+        <Cards
+          class="col-md-4"
+          src="https://picsum.photos/1080"
+          alt="imagen1"
+          Cardtitle="Producto 1"
+          text="80 USD"
+          href=""
+          textButton="Ver Producto"
+        />
       </section>
     </>
   );
