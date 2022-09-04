2bd0681405e00f2552c95224144c5f05bb5bcd52
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-09-04 10:49:50 -0500

    Se repara la posicion del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <footer className="bg-white">
-        <div className="bg-dark py-2 footerposition">
+        <div className="bg-dark py-2 ">
           <div className="text-center">
             <p className="text-muted mb-0 py-2">
               © 2022 All rights reserved. create by Cesar Reyes
