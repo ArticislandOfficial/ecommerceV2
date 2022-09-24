6fea39ec84381b5909ae5fa7b6d725c00dfae41f
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-09-24 16:48:54 -0500

    productContext

--- src/Pages/Productos.jsx
+++ src/Pages/Productos.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import ProductsCards from "../components/ProductsCards";
+import ProductsList from "../components/ProductsList";
 
 const Productos = () => {
   return (
     <>
       <section className="row d-flex justify-content-evenly">
         <h3 className="my-3 text-center">Productos y Servicios</h3>
-        <ProductsCards/>
+        <ProductsList/>
        
       </section>
     </>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Cards = (props) => {
   return (
     <>
       <div className={props.class}>
         <section className="card estiloextcard  my-3 ">
-          <img src={props.src} className="card-img-top" alt={props.alt} />
+          <img srcSet={props.src} className="card-img-top" alt={props.alt} />
           <article className="card-body text-center">
             <h5 className="card-title">{props.Cardtitle}</h5>
             <p className="card-text">{props.text}</p>
             <NavLink to="/producto">
-              {/* <a href={props.href} className="btn btn-primary">
-                {props.textButton}
-              </a> */}
+              <button className="btn btn-primary">ver producto</button>
             </NavLink>
           </article>
         </section>
       </div>
     </>
   );
-}
+};
 
-export default Cards
+export default Cards;
