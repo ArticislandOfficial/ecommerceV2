14f6dfa2703935707c9e7d8db1e6a2d9c5452c18
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-09-27 21:40:10 -0500

    Paypal

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React from "react";
 import AppRouter from "../src/routers/AppRouter";
 import Footer from "./components/Footer";
 import { AuthProvider } from "./context/AuthContext";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import ProductState from "./context/ProductState";
+
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import ProductState from "./context/ProductState";
+
 
 const App = () => {
   return (
     <>
       <AuthProvider>
-        <ProductState>
-          <AppRouter />
-          <Footer />
-        </ProductState>
+        <PayPalScriptProvider
+          options={{
+            "client-id":
+              "AVxE4OsPYQFvrnhcDPeu1x78puMjbmXWKJM4bXqIJF3t86N46keduJHroaUFzly_eVB5Vd9X9zBI1p4n",
+          }}
+        >
+          <ProductState>
+            <AppRouter />
+            <Footer />
+          </ProductState>
+        </PayPalScriptProvider>
       </AuthProvider>
     </>
   );
