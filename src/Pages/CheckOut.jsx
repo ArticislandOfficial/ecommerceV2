7b0ed07a31bc6d404aa0a4e81502bf083663f5f0
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-09-27 22:05:40 -0500

    sgregaando reducer al Carrito

--- src/Pages/CheckOut.jsx
+++ src/Pages/CheckOut.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import CartList from '../components/CartList';
+import React, { useState, useContext, useEffect } from "react";
+import ProductContext from "../context/ProductContext";
+import CartList from "../components/CartList";
 
 const CheckOut = () => {
-  return <div className="cont-posfotter-abajo mt-5 pt-5">CheckOut
-  <CartList/>
-  
-  
-  </div>;
-}
+  const { cart } = useContext(ProductContext);
+  const [total, setTotal] = useState([]);
+useEffect(() => {
+  setTotal(cart.reduce((acc, producto) => acc + producto.price, 0));
+}, [cart]);
+  return (
+    <>
+      <main className="row cont-posfotter-abajo mt-5 pt-5 pt-5">
+        <h3>Carrito </h3>
+        <article className="col-md-12">
+          <CartList />
+        </article>
+        <section className="row pt-5">
+          <article className="col-md-12">
+            <h2>{`El total es : $${total}MXN`}</h2>
+          </article>
+        </section>
+      </main>
+    </>
+  );
+};
 
-export default CheckOut
+export default CheckOut;
